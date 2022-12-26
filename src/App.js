2302301ace87675bed0b4b086c348ec5815b939b
ntnfolla <101428946+ntnfolla@users.noreply.github.com>
2022-12-26 13:53:53 +0300

    updates

---
+++ src/App.js
@@ -0,0 +1,23 @@
+
+function App() {
+    return (
+        <div className="App">
+            <header ClassName="App-header">
+                <img src={logo} className="app-logo" alt="logo" />
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <a 
+                ClassName="App-link"
+                href="https://reactjs.org"
+                target="_blank"
+                rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+            </header>
+        </div>
+    );
+}
+
+export default App;
