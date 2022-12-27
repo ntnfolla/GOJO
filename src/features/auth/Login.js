e96169b5acdf7431a9f60503050fcf6e6f9606b1
ntnfolla <101428946+ntnfolla@users.noreply.github.com>
2022-12-27 16:30:29 +0300

    Gojo 04

--- src/features/auth/Login.js
+++
@@ -1,6 +0,0 @@
-const login = () => {
-    return (
-        <h1>login</h1>
-    )
-}
-export default login
--- src/features/houses/HousesList.js
+++
@@ -1,6 +0,0 @@
-const HousesList = () => {
-    return (
-        <h1>HousesList</h1>
-    )
-}
-export default HousesList
